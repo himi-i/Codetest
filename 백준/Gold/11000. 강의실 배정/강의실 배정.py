078b3191dcc7b3170a9cf681d03ec1970dd25b9e
himi-i <114150749+himi-i@users.noreply.github.com>
2026-02-27 22:25:45 +0900

    [Gold IV] Title: 강의실 배정, Time: 332 ms, Memory: 74744 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+import heapq
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+arr.sort()
+
+heap = []
+heapq.heappush(heap, arr[0][1])
+
+for i in range(1, n):
+    start, end = arr[i]
+
+    if heap[0] <= start:
+        heapq.heappop(heap)
+    heapq.heappush(heap, end)
+
+print(len(heap))
+
+    
+    
+
+
+
+
+    
