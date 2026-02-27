c609e1d27f9214cb41b226f0afa317ca81270a94
himi-i <114150749+himi-i@users.noreply.github.com>
2026-02-27 15:45:41 +0900

    [Bronze II] Title: Hashing, Time: 48 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -5,8 +5,10 @@ N = int(input())
 
 arr = input().strip()
 count = 0
+M = 1234567891
+
 for i, ch in enumerate(arr):
     x = ord(ch) - 96
     count += x * (31 ** i)
 
-print(count)
+print(count % M)
